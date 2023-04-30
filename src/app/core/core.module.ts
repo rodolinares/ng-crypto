df62b68f0fdf71d4230b97e5e34574b2a1f585ae
Rodrigo Linares <rodolinares19@gmail.com>
2023-04-30 00:59:18 -0500

    add hover to card

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,12 +3,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 import { LayoutComponent } from './components/layout/layout.component';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, RouterModule, NzLayoutModule],
+  imports: [CommonModule, RouterModule, NzLayoutModule, NzTypographyModule],
   exports: [LayoutComponent]
 })
 export class CoreModule {}
